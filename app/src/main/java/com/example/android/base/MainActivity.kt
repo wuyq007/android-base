5ba15d26b1026fe7a6a5b4436691ac5e72c72409
wuyq <664287291@qq.com>
2023-03-14 14:42:15 +0800

    DataStoreUtils

--- app/src/main/java/com/example/android/base/MainActivity.kt
+++ app/src/main/java/com/example/android/base/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.android.base
 
 import android.os.Bundle
+import android.util.Log
 import com.example.android.base.databinding.ActivityMainBinding
 import com.pers.base.lib.BaseActivity
+import com.pers.base.lib.utils.DataStoreUtils
+import kotlinx.coroutines.launch
 
 class MainActivity : BaseActivity() {
 
@@ -13,6 +16,41 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        launch {
+            DataStoreUtils.apply {
+                saveInt("testInt", 18)
+                saveFloat("testFloat", 18F)
+                saveDouble("testDouble", 18.0)
+                saveLong("testLong", 18L)
+                saveBoolean("testBoolean", true)
+                saveString("testString", "哈哈哈哈")
+            }
+
+            DataStoreUtils.apply {
+                Log.e("AAA", "is testInt:${contains("testInt")}")
+                Log.e("AAA", "is testFloat:${contains("testFloat")}")
+                Log.e("AAA", "is testDouble:${contains("testDouble")}")
+                Log.e("AAA", "is testLong:${contains("testLong")}")
+                Log.e("AAA", "is testBoolean:${contains("testBoolean")}")
+                Log.e("AAA", "is testString:${contains("testString")}")
+
+                val testInt =getInt("testInt")
+                val testFloat = getFloat("testFloat")
+                val testDouble = getDouble("testDouble")
+                val testLong = getLong("testLong")
+                val testBoolean = getBoolean("testBoolean")
+                val testString = getString("testString")
+//
+                Log.e("AAA", "testInt:$testInt")
+                Log.e("AAA", "testFloat:$testFloat")
+                Log.e("AAA", "testDouble:$testDouble")
+                Log.e("AAA", "testLong:$testLong")
+                Log.e("AAA", "testBoolean:$testBoolean")
+                Log.e("AAA", "testString:$testString")
+            }
+
+        }
 
     }
 
--- libs/base-lib/src/main/java/com/pers/base/lib/utils/DataStoreUtils.kt
+++ libs/base-lib/src/main/java/com/pers/base/lib/utils/DataStoreUtils.kt
@@ -5,12 +5,11 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.*
 import androidx.datastore.preferences.preferencesDataStore
 import com.pers.base.lib.AppConfig
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.*
+import kotlin.properties.Delegates
 
 // 文件名称: /data/data/项目包名/files/下创建名为 dataStore_data 的文件
-val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "dataStore_data")
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "data_store_data")
 
 object DataStoreUtils {
 
@@ -23,12 +22,16 @@ object DataStoreUtils {
         }
     }
 
-    suspend fun getInt(key: String): Int {
-        return context().dataStore.data.map {
-            it[intPreferencesKey(key)] ?: 0
-        }.first()
+    suspend fun getInt(key: String): Int? {
+        return try {
+            dataStore.data.map {
+                it[intPreferencesKey(key)]
+            }.firstOrNull()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
     }
-
 
     suspend fun saveFloat(key: String, value: Float) {
         dataStore.edit {
@@ -36,10 +39,15 @@ object DataStoreUtils {
         }
     }
 
-    suspend fun getFloat(key: String): Float {
-        return dataStore.data.map { preferences ->
-            preferences[floatPreferencesKey(key)] ?: 0F
-        }.first()
+    suspend fun getFloat(key: String): Float? {
+        return try {
+            dataStore.data.map {
+                it[floatPreferencesKey(key)]
+            }.firstOrNull()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
     }
 
     suspend fun saveDouble(key: String, value: Double) {
@@ -48,12 +56,16 @@ object DataStoreUtils {
         }
     }
 
-    suspend fun getDouble(key: String): Double {
-        return dataStore.data.map { preferences ->
-            preferences[doublePreferencesKey(key)] ?: 0.0
-        }.first()
+    suspend fun getDouble(key: String): Double? {
+        return try {
+            dataStore.data.map {
+                it[doublePreferencesKey(key)]
+            }.firstOrNull()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
     }
-
 
     suspend fun saveLong(key: String, value: Long) {
         dataStore.edit {
@@ -61,12 +73,16 @@ object DataStoreUtils {
         }
     }
 
-    suspend fun getLong(key: String): Long {
-        return dataStore.data.map { preferences ->
-            preferences[longPreferencesKey(key)] ?: 0L
-        }.first()
+    suspend fun getLong(key: String): Long? {
+        return try {
+            dataStore.data.map {
+                it[longPreferencesKey(key)]
+            }.firstOrNull()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
     }
-
 
     suspend fun saveBoolean(key: String, value: Boolean) {
         dataStore.edit {
@@ -74,12 +90,16 @@ object DataStoreUtils {
         }
     }
 
-    suspend fun getBoolean(key: String): Boolean {
-        return dataStore.data.map { preferences ->
-            preferences[booleanPreferencesKey(key)] ?: false
-        }.first()
+    suspend fun getBoolean(key: String): Boolean? {
+        return try {
+            dataStore.data.map {
+                it[booleanPreferencesKey(key)]
+            }.firstOrNull()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
     }
-
 
     suspend fun saveString(key: String, value: String) {
         dataStore.edit {
@@ -87,12 +107,16 @@ object DataStoreUtils {
         }
     }
 
-    suspend fun getString(key: String): String {
-        return dataStore.data.map { preferences ->
-            preferences[stringPreferencesKey(key)] ?: ""
-        }.first()
+    suspend fun getString(key: String): String? {
+        return try {
+            dataStore.data.map {
+                it[stringPreferencesKey(key)]
+            }.firstOrNull()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
     }
-
 
     suspend fun saveStringSet(key: String, value: Set<String>) {
         dataStore.edit {
@@ -100,11 +124,174 @@ object DataStoreUtils {
         }
     }
 
-    suspend fun getStringSet(key: String): Set<String> {
-        val flow: Flow<Set<String>> = dataStore.data.map { preferences ->
-            preferences[stringSetPreferencesKey(key)] ?: HashSet<String>()
+    suspend fun getStringSet(key: String): Set<String>? {
+        return try {
+            dataStore.data.map {
+                it[stringSetPreferencesKey(key)]
+            }.firstOrNull()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
         }
-        return flow.first()
+    }
+
+    suspend fun removeInt(key: String) {
+        dataStore.edit {
+            it.remove(intPreferencesKey(key))
+        }
+    }
+
+    suspend fun removeFloat(key: String) {
+        dataStore.edit {
+            it.remove(floatPreferencesKey(key))
+        }
+    }
+
+    suspend fun removeDouble(key: String) {
+        dataStore.edit {
+            it.remove(doublePreferencesKey(key))
+        }
+    }
+
+    suspend fun removeLong(key: String) {
+        dataStore.edit {
+            it.remove(longPreferencesKey(key))
+        }
+    }
+
+    suspend fun removeBoolean(key: String) {
+        dataStore.edit {
+            it.remove(booleanPreferencesKey(key))
+        }
+    }
+
+    suspend fun removeString(key: String) {
+        dataStore.edit {
+            it.remove(stringPreferencesKey(key))
+        }
+    }
+
+    suspend fun removeStringSet(key: String) {
+        dataStore.edit {
+            it.remove(stringSetPreferencesKey(key))
+        }
+    }
+
+    suspend fun containsInt(key: String): Boolean {
+        var isContains by Delegates.notNull<Boolean>()
+        dataStore.edit {
+            isContains = it.contains(intPreferencesKey(key))
+        }
+        return isContains
+    }
+
+    suspend fun containsFloat(key: String): Boolean {
+        var isContains by Delegates.notNull<Boolean>()
+        dataStore.edit {
+            isContains = it.contains(floatPreferencesKey(key))
+        }
+        return isContains
+    }
+
+    suspend fun containsDouble(key: String): Boolean {
+        var isContains by Delegates.notNull<Boolean>()
+        dataStore.edit {
+            isContains = it.contains(doublePreferencesKey(key))
+        }
+        return isContains
+    }
+
+    suspend fun containsLong(key: String): Boolean {
+        var isContains by Delegates.notNull<Boolean>()
+        dataStore.edit {
+            isContains = it.contains(longPreferencesKey(key))
+        }
+        return isContains
+    }
+
+    suspend fun containsBoolean(key: String): Boolean {
+        var isContains by Delegates.notNull<Boolean>()
+        dataStore.edit {
+            isContains = it.contains(booleanPreferencesKey(key))
+        }
+        return isContains
+    }
+
+    suspend fun containsString(key: String): Boolean {
+        var isContains by Delegates.notNull<Boolean>()
+        dataStore.edit {
+            isContains = it.contains(stringPreferencesKey(key))
+        }
+        return isContains
+    }
+
+    suspend fun containsStringSet(key: String): Boolean {
+        var isContains by Delegates.notNull<Boolean>()
+        dataStore.edit {
+            isContains = it.contains(stringSetPreferencesKey(key))
+        }
+        return isContains
+    }
+
+    suspend fun <T> save(key: Preferences.Key<T>, value: T) {
+        dataStore.edit {
+            it[key] = value
+        }
+    }
+
+    suspend fun <T> get(key: Preferences.Key<T>): T? {
+        return try {
+            dataStore.data.map { it ->
+                it[key]
+            }.firstOrNull()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
+    }
+
+    suspend fun <T> contains(key: Preferences.Key<T>): Boolean {
+        var isContains by Delegates.notNull<Boolean>()
+        dataStore.edit {
+            isContains = it.contains(key)
+        }
+        return isContains
+    }
+
+    suspend fun <T> remove(key: Preferences.Key<T>) {
+        dataStore.edit {
+            it.remove(key)
+        }
+    }
+
+    /**
+     * contains 时不用管存的什么类型
+     */
+    suspend fun contains(key: String): Boolean {
+        var isContains by Delegates.notNull<Boolean>()
+        dataStore.edit {
+            isContains = it.contains(stringPreferencesKey(key))
+        }
+        return isContains
+    }
+
+
+    /**
+     * remove 时不用管存的什么类型，都会删除掉
+     */
+    suspend fun remove(key: String) {
+        dataStore.edit {
+            it.remove(stringPreferencesKey(key))
+        }
+    }
+
+    /**
+     * 清除全部数据
+     */
+    suspend fun clear() {
+        dataStore.edit {
+            it.clear()
+        }
     }
 
 }
