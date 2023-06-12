bb8ff5158ff4a3ea726e3ce7b9dedea89e4f12a4
wuyq <664287291@qq.com>
2023-06-12 10:47:44 +0800

    SupervisorJob

--- libs/base-lib/src/main/java/com/pers/libs/base/BaseFragment.kt
+++ libs/base-lib/src/main/java/com/pers/libs/base/BaseFragment.kt
@@ -2,21 +2,22 @@ package com.pers.libs.base
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import kotlinx.coroutines.CompletableJob
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
 import kotlin.coroutines.CoroutineContext
 
 open class BaseFragment : Fragment(), CoroutineScope {
 
-    private lateinit var job: Job
+    private lateinit var job: CompletableJob
     override val coroutineContext: CoroutineContext
         get() = job + Dispatchers.Main
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        job = Job()
+        job = SupervisorJob()
     }
 
     override fun onDestroy() {
